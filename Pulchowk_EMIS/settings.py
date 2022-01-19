3923120ce4dc2917098ddfa7d48bbe5bafbb8b7e
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-01-19 17:13:08 +0545

    Apps initialized

--- Pulchowk_EMIS/settings.py
+++ Pulchowk_EMIS/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'customuser.apps.CustomuserConfig',
+    'staff.apps.StaffConfig',
+    'student.apps.StudentConfig',
 ]
 
 MIDDLEWARE = [
