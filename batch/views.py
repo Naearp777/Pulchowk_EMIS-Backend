d98f3244c7c1d7390ee89b54d47b2f9a7f64af3e
Saugat <saugatkafley@gmail.com>
2022-01-21 11:25:43 +0545

    Batch model views (show_batch)

--- batch/views.py
+++ batch/views.py
@@ -5,10 +5,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import  IsAuthenticated
 from rest_framework.response import Response
-# Create your views here.
 
+# Create your views here.
 @api_view(['GET'])
 def show_batch(request):
-    if request.method == 'GET':
-        batch = BatchSerializer(Batch.objects.all(), many=True)
-        return Response(batch.data)
+    batch = BatchSerializer(Batch.objects.all(), many=True)
+    return Response(batch.data)
