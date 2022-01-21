2041534e61947d3a2416a6ef4d9f3490af2ed930
Saugat <saugatkafley@gmail.com>
2022-01-21 12:55:55 +0545

    Add batch

--- batch/urls.py
+++ batch/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('api/show_batch', views.show_batch ),
+    path('api/show_batch', views.show_batch , name = "show_batch" ),
+    path('api/add_batch', views.add_batch , name = "add_batch" ),
+    path('api/destroy_batch/<int:pk>', views.DestroyBatch.as_view()  ),
 ]
--- batch/views.py
+++ batch/views.py
@@ -5,9 +5,19 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import  IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework import generics
 # Create your views here.
 @api_view(['GET']) 
 def show_batch(request):
     batch = BatchSerializer(Batch.objects.all(), many=True)
     return Response(batch.data)
+
+@api_view(['POST'])
+def add_batch(request):
+    batch  = BatchSerializer(Batch.objects.create() , many = False)
+    return Response(batch.data)
+
+class DestroyBatch(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
+    
