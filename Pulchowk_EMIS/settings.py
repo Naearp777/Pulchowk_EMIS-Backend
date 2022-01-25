9e4c1ed9e9839acc867c6e1e5e8a782114cd3cda
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-01-25 10:25:28 +0545

    Deployment processing

--- Pulchowk_EMIS/settings.py
+++ Pulchowk_EMIS/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -124,32 +124,35 @@ WSGI_APPLICATION = 'Pulchowk_EMIS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # DATABASES = {
+
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#         'ENGINE': 'django.db.backends.postgresql',
+
+#         'NAME': 'Pulchowk_EMIS',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'admin',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
 #     }
+
 # }
 
-DATABASES = {
-
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME': 'Pulchowk_EMIS',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'admin',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-    }
-
-}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
