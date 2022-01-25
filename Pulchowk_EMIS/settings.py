eea9f1cdd38b814fa1a8d25910dc61109d84cac3
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-01-25 10:33:12 +0545

    allowed host changed

--- Pulchowk_EMIS/settings.py
+++ Pulchowk_EMIS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--*#h$71mryy$tg!h1p^rb(hvhn=*mxu&oa7(3bltaxu3!ds8hd
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pulchowk-emis.herokuapp.com','*']
 
 ALLOWED_HOSTS = []
 
