9f5b4ed69067dc4890efcd5ebccb6a805d2e63aa
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-02 11:16:46 +0545

    Custom user registation

--- Pulchowk_EMIS/settings.py
+++ Pulchowk_EMIS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'Pulchowk_EMIS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -154,6 +155,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'volunteermanagementsoftware@gmail.com'
+EMAIL_HOST_PASSWORD = 'apqkfwpmhdqtwpee'
+DEFAULT_FROM_EMAIL = 'Givers Team <noreply@volunteermanagementsystem.com>'
 
 MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
--- customuser/views.py
+++ customuser/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+from django.core.mail.message import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def registerUser(request):
+    
+    data = request.data
+    print(data)
+    try:
+        user = User.objects.create(
+            password=make_password(data['password']),
+            email=data['email'],
+            first_name=data['first_name'],
+            middle_name=data['middle_name'],
+            last_name=data['last_name'],
+            roll_no=data['roll_no'],
+            address=data['address'],
+            gender=data['gender'],
+            phone=data['phone'],
+            dob=data['dob'],
+            admin=data['admin'],
+            student=data['student'],
+            department=data['department'],
+            staff=data['staff'],
+
+        )
+        
+        user = User.objects.get(email=data['email'])
+        user.images = request.FILES.get('images')
+        user.save()
+        serializer = UserSerializer(user, many=False)
+
+        email_template = render_to_string('signup.html', {"first_name":serializer.data['first_name'],
+                                           "password": serializer.data['password'], "email": serializer.data['email']})
+        sign_up = EmailMultiAlternatives(
+            "Account has been Created",
+            "Account has been Created",
+            settings.EMAIL_HOST_USER,
+            [serializer.data['email']],
+        )
+        sign_up.attach_alternative(email_template, 'text/html')
+        sign_up.send()
+
+        return Response(serializer.data)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
