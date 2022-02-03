63c427c6d5b493c93a22aa98d363f0e39d499ab7
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-03 17:01:58 +0545

    URLS added

--- Pulchowk_EMIS/urls.py
+++ Pulchowk_EMIS/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', include("authentication.urls")),
     path('', include("customuser.urls")),
     path('', include("classes.urls")),
+    path('', include("department.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
