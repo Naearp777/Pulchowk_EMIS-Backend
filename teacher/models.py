8106b04c8b58458fc8b77a88cf99ed8d16e77c6d
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-12 22:03:35 +0545

    teacher added

--- teacher/models.py
+++ teacher/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from department.models import department
+from customuser.models import User
 # Create your models here.
 class Teachers_info(models.Model):
+    teacher=models.ForeignKey(User,on_delete=models.CASCADE)
     department=models.ForeignKey(department,on_delete=models.CASCADE)
     
     def __str__(self):
--- teacher/serializers.py
+++ teacher/serializers.py
@@ -3,9 +3,10 @@ from rest_framework import  serializers
 from department.serializers import DepartmentSerializer
 
 from .models import Teachers_info
-
+from customuser.serializers import UserSerializer_std
 
 class TeacherSerializer(serializers.ModelSerializer):
+    teacher=UserSerializer_std(read_only=True, many=False)
     department=DepartmentSerializer(read_only=True, many=False)
     class Meta:
         model = Teachers_info
