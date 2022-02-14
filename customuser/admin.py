1cbbb3993b17f374584f45eea918184f80cff398
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-14 09:51:25 +0545

    Field setes added

--- customuser/admin.py
+++ customuser/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import User,ExcelFileUpload
 # Register your models here.
-admin.site.register(User)
+
 admin.site.register(ExcelFileUpload)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets=(
+        ('Login',{'fields':('email','password')}),
+        ('Personal Info',{'fields':('first_name','middle_name','last_name','address')}),
+        ('Contact Info',{'fields':('phone','dob')}),
+        ('Profile Image',{'fields':('images',)}),
+        ('Permissions',{'fields':('admin','staff','student')}),
+        ('Other Info',{'fields':('password_changed','last_login')}),
+    )
+
+admin.site.register(User,CustomUserAdmin)
