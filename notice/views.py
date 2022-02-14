6c2164943f0232819e552dfee0bc78d809e96a0c
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-14 21:18:51 +0545

    nested serializers for all views

--- notice/views.py
+++ notice/views.py
@@ -1,8 +1,8 @@
 from urllib import response
 from django.shortcuts import render
 
-from notice.serializers import NoticeSerializer
-from .models import notice
+from notice.serializers import NoticeSerializer,GlobalNoticeSerializer
+from .models import notice,global_notice
 from classes.models import classes
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -41,7 +41,7 @@ def show_notice(request,c_id):
 def create_globalnotice(request):
   data=request.data
   try:
-    notice.objects.create(
+    global_notice.objects.create(
       title=data['title'],
       content=data['content'],
       files=request.FILES.get('files'),
@@ -56,8 +56,8 @@ def create_globalnotice(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def show_globalnotice(request,pk):
-  shownotice=notice.objects.filter(publish_to=pk)
-  serializer = NoticeSerializer(shownotice, many=True)
+  shownotice=global_notice.objects.filter(publish_to=pk)
+  serializer = GlobalNoticeSerializer(shownotice, many=True)
   return Response(serializer.data)
 
 @api_view(['DELETE'])
@@ -90,7 +90,7 @@ def update_notice(request,pk):
 @permission_classes([IsAuthenticated])
 def delete_globalnotice(request,pk):
   try:
-    notice.objects.get(id=pk).delete()
+    global_notice.objects.get(id=pk).delete()
     return Response(status=status.HTTP_200_OK)
   except:
     return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -99,7 +99,7 @@ def delete_globalnotice(request,pk):
 @permission_classes([IsAuthenticated])
 def update_globalnotice(request,pk):
   try:
-    update_notice=notice.objects.get(id=pk)
+    update_notice=global_notice.objects.get(id=pk)
     data=request.data
     update_notice.title=data['title']
     update_notice.content=data['content']
