9f1b6125e0779579857e07fd7dd25dc1ef00b29a
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-14 16:21:17 +0545

    notice urls added

--- notice/urls.py
+++ notice/urls.py
@@ -3,4 +3,12 @@ from .import views
 urlpatterns = [
 
     path('api/notice/create/<int:c_id>/', views.createnotice_teacher, name='createnotice'),
+    path('api/notice/show/<int:c_id>/', views.show_notice, name='show_notice'),
+    path('api/notice/global/create/', views.create_globalnotice, name='create_globalnotice'),
+    path('api/notice/show/global/<int:pk>/', views.show_globalnotice, name='show_globalnotice'),
+    path('api/notice/update/<int:pk>/', views.update_notice, name='update_notice'),
+    path('api/notice/delete/<int:pk>/', views.delete_notice, name='delete_notice'),
+    path('api/notice/global/delete/<int:pk>/', views.delete_globalnotice, name='delete_globalnotice'),
+    path('api/notice/global/update/<int:pk>/', views.update_globalnotice, name='update_globalnotice'),
+  
 ]
