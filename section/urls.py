89cfc09fba4a53b8d17aed91b82196d01b4ebdb4
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-14 17:12:57 +0545

    exception handling added now

--- section/urls.py
+++ section/urls.py
@@ -3,4 +3,5 @@ from .import views
 urlpatterns = [
 
     path('api/view/section/', views.section_display_all, name='view_section'),
+    path('api/create/section/', views.section_create, name='create_section'),
 ]
