800488da85261b168d7d72bf458c37a719da6cfa
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-14 16:16:29 +0545

    notice views added

--- notice/models.py
+++ notice/models.py
@@ -16,6 +16,22 @@ class notice(models.Model):
         verbose_name_plural = "Notices"
 
 
+class global_notice(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    files=models.FileField(upload_to='notice/files/',blank=True)
+    publish_to=models.ForeignKey(User,on_delete=models.CASCADE)
+    publish_by=models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Meta:
+        verbose_name_plural = "Global Notices"
+
+
+
+
 
     
 
--- notice/views.py
+++ notice/views.py
@@ -1,5 +1,7 @@
 from urllib import response
 from django.shortcuts import render
+
+from notice.serializers import NoticeSerializer
 from .models import notice
 from classes.models import classes
 from rest_framework.decorators import api_view
@@ -25,3 +27,78 @@ def createnotice_teacher(request,c_id):
   except:
     return Response(status=status.HTTP_400_BAD_REQUEST)
         
+@api_view(['GET'])
+def show_notice(request,c_id):
+  shownotice=notice.objects.filter(publish_to=c_id)
+  serializer = NoticeSerializer(shownotice, many=True)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def create_globalnotice(request):
+  data=request.data
+  try:
+    notice.objects.create(
+      title=data['title'],
+      content=data['content'],
+      files=request.FILES.get('files'),
+      publish_to=User.objects.all(),
+      publish_by=data['publish_by']
+
+    )
+    return Response(status=status.HTTP_201_CREATED)
+  except:
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def show_globalnotice(request,pk):
+  shownotice=notice.objects.filter(publish_to=pk)
+  serializer = NoticeSerializer(shownotice, many=True)
+  return Response(serializer.data)
+
+@api_view(['DELETE'])
+def delete_notice(request,pk):
+  try:
+    notice.objects.get(id=pk).delete()
+    return Response(status=status.HTTP_200_OK)
+  except:
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update_notice(request,pk):
+  try:
+    update_notice=notice.objects.get(id=pk)
+    data=request.data
+    update_notice.title=data['title']
+    update_notice.content=data['content']
+    update_notice.files=request.FILES.get('files')
+    update_notice.publish_to=classes.objects.get(id=data['publish_to'])
+    update_notice.publish_by=data['publish_by']
+    update_notice.save()
+    return Response(status=status.HTTP_200_OK)
+  except:
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_globalnotice(request,pk):
+  try:
+    notice.objects.get(id=pk).delete()
+    return Response(status=status.HTTP_200_OK)
+  except:
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update_globalnotice(request,pk):
+  try:
+    update_notice=notice.objects.get(id=pk)
+    data=request.data
+    update_notice.title=data['title']
+    update_notice.content=data['content']
+    update_notice.files=request.FILES.get('files')
+    update_notice.publish_to=User.objects.get(id=data['publish_to'])
+    update_notice.publish_by=data['publish_by']
+    update_notice.save()
+    return Response(status=status.HTTP_200_OK)
+  except:
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
