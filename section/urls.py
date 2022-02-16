838d65b68f5f9d14e5fed55bac830082db9fe752
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-16 09:41:54 +0545

    section crud operation added

--- section/urls.py
+++ section/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
 
     path('api/view/section/', views.section_display_all, name='view_section'),
     path('api/create/section/', views.section_create, name='create_section'),
+    path('api/update/section/<int:id>/', views.section_update, name='update_section'),
+    path('api/delete/section/<int:id>/', views.section_delete, name='delete_section'),
 ]
--- section/views.py
+++ section/views.py
@@ -24,3 +24,25 @@ def section_create(request):
         return Response(status=status.HTTP_201_CREATED)
     except Exception as e:
         return Response({"message":str(e)},status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def section_update(request,id):
+    data=request.data
+    try:
+        section.objects.filter(id=id).update(
+            name=data['name'],
+        )
+        return Response(status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response( {"message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def section_delete(request,id):
+    try:
+        section.objects.filter(id=id).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response( {"message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
