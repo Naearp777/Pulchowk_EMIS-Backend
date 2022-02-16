102e9c4e43e5a3d9973d548dacf9d48ec198194a
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-02-16 09:40:34 +0545

    batch crud operation added

--- batch/urls.py
+++ batch/urls.py
@@ -3,4 +3,7 @@ from .import views
 urlpatterns = [
 
     path('api/view/batch/', views.Batch_display_all, name='view_batch'),
+    path('api/create/batch/', views.Batch_create, name='create_batch'),
+    path('api/update/batch/<int:id>/', views.Batch_update, name='update_batch'),
+    path('api/delete/batch/<int:id>/', views.Batch_delete, name='delete_batch'),
 ]
--- batch/views.py
+++ batch/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import batch
 from .serializers import BatchSerializer
 from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -12,3 +13,36 @@ def Batch_display_all(request):
     all_batch = batch.objects.all()
     serializer = BatchSerializer(all_batch, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def Batch_create(request):
+    data=request.data
+    try:
+        batch.objects.create(
+            name=data['name'],
+        )
+        return Response(status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response( {"message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def Batch_update(request,id):
+    data=request.data
+    try:
+        batch.objects.filter(id=id).update(
+            name=data['name'],
+        )
+        return Response(status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response( {"message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)  
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def Batch_delete(request,id):
+    try:
+        batch.objects.filter(id=id).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response( {"message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
