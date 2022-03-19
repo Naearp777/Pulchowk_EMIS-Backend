3718329ff66fbb1cb18b9da57c5992ef23fa691a
Chirag Lamsal <chirag.lamsal@gmail.com>
2022-03-19 12:38:10 +0545

    Add get student profile by id

--- customuser/views.py
+++ customuser/views.py
@@ -56,7 +56,11 @@ def registerUser(request):
                 department=department.objects.get(name=data["department_name"]),
                 batch=batch.objects.get(name=data["batch"]),
                 section=section.objects.get(name=data["section"]),
-                full_name=user.first_name + " " + user.middle_name + " " + user.last_name,
+                full_name=user.first_name
+                + " "
+                + user.middle_name
+                + " "
+                + user.last_name,
             )
         if serializer.data["staff"] == True:
             Teachers_info.objects.create(
--- student/views.py
+++ student/views.py
@@ -1,8 +1,9 @@
 from student.models import student_info
-from .serializers import StudentSerializer,StudentallSerializer
+from .serializers import StudentSerializer, StudentallSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -13,6 +14,21 @@ def show_all_students(request):
     all_students = student_info.objects.all()
     serializer = StudentSerializer(all_students, many=True)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def show_student_profile(request, s_id):
+    try:
+        student = student_info.objects.get(student_id=s_id)
+        print("student")
+        serializer = StudentSerializer(student, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        return Response(
+            {"message": "Student not found"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
