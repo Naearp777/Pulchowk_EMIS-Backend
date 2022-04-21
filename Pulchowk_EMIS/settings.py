f4ff3a84e2dd5fcc0e7b89f66ab9223562b65eef
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-04-21 08:53:06 +0545

    DB changed to Postgres

--- Pulchowk_EMIS/settings.py
+++ Pulchowk_EMIS/settings.py
@@ -149,16 +149,16 @@ WSGI_APPLICATION = "Pulchowk_EMIS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
